3cf67e360bb2f36cf7a89b6d1b7528f363322371
vavramova <avramovaviki017@gmail.com>
2026-04-24 22:47:10 +0300

    built first ever express server with routes

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+
+//creaed the server
+const app = express()
+app.use(express.json())
+
+
+//when visit server`s homepage, run:
+app.get('/', (req, res) =>{
+    // .send -> Plaintext
+    res.send('Password manager API is running!')
+})
+
+
+// .get -> fetch data
+app.get('/health', (req, res) => {
+    // .json -> sends JSON Data => Frontend expects JSON
+    res.json({ status: 'ok', message: 'Server is healthy'})
+})
+
+
+// .post -> send data
+app.post('/test', (req, res) => {
+    const body = req.body
+    res.json({ received: body })
+})
+
+app.listen(3000, () => {
+    console.log('Server started on Port 3000')
+})
